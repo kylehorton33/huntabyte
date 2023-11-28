006f9c0b8ff78dc51491f1d94e310e9022948dbd
Kyle Horton <kyle@cplventures.com>
2023-11-28 16:37:22 -0500

    VSCode format on save: false

--- web/src/routes/projects/new/+page.server.js
+++ web/src/routes/projects/new/+page.server.js
@@ -1,30 +1,29 @@
-import { error, redirect } from "@sveltejs/kit"
-
+import { error, redirect } from "@sveltejs/kit";
 
 export const load = ({ locals }) => {
-    if (!locals.pb.authStore.isValid) {
-        throw redirect(303, '/login')
-    }
-}
+  if (!locals.pb.authStore.isValid) {
+    throw redirect(303, "/login");
+  }
+};
 
 export const actions = {
-    create: async({ request, locals }) => {
-        const formData = await request.formData()
+  create: async ({ request, locals }) => {
+    const formData = await request.formData();
 
-        const thumbnail = formData.get('thumbnail')
+    const thumbnail = formData.get("thumbnail");
 
-        if (thumbnail.size === 0) {
-            formData.delete('thumbnail')
-        }
-
-        formData.append('user', locals.user.id)
-
-        try {
-            await locals.pb.collection('projects').create(formData)
-        } catch (err) {
-            throw error(err.status, err.message)
-        }
-
-        throw redirect(303, '/')
+    if (thumbnail.size === 0) {
+      formData.delete("thumbnail");
     }
-}
+
+    formData.append("user", locals.user.id);
+
+    try {
+      await locals.pb.collection("projects").create(formData);
+    } catch (err) {
+      throw error(err.status, err.message);
+    }
+
+    throw redirect(303, "/");
+  },
+};
