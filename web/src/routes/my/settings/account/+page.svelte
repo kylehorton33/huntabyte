a2cd3a8c450b4a3786049eaa55e642f652540269
Kyle Horton <kyle@cplventures.com>
2023-11-28 16:05:40 -0500

    implement change e-mail from video#3. stop there. no security needed

---
+++ web/src/routes/my/settings/account/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Input, Modal } from "$lib/components";
+  
+    export let form;
+  
+    let emailModalOpen;
+    let usernameModalOpen;
+  
+    $: emailModalOpen = false;
+    $: usernameModalOpen = false;
+  </script>
+  
+  <div class="flex flex-col w-full h-full space-y-12">
+    <div class="w-full">
+      <h3 class="text-2xl font-medium">Change Email</h3>
+      <div class="divider" />
+    </div>
+    <Modal label="change-email" checked={emailModalOpen}>
+      <span slot="trigger" class="btn btn-primary">Change Email</span>
+      <h3 slot="heading">Change Your Email</h3>
+      <form action="?/updateEmail" method="POST" class="space-y-2">
+          <Input
+              id="email"
+              type="email"
+              required="true"
+              label="Enter your new email address:"
+              value={form?.data?.email}
+          />
+          <button type="submit" class="btn btn-primary w-full">Change My Email</button>
+      </form>
+    </Modal>
+  </div>
+  
---
+++ web/src/routes/my/settings/security/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Input, Modal } from "$lib/components";
+
+  export let form;
+
+  let emailModalOpen;
+
+  $: emailModalOpen = false;
+</script>
+
+<div class="flex flex-col w-full h-full space-y-12">
+  <div class="w-full">
+    <h3 class="text-2xl font-medium">Change Email</h3>
+    <div class="divider" />
+  </div>
+  <Modal label="change-email" checked={emailModalOpen}>
+    <span slot="trigger" class="btn btn-primary">Change Email</span>
+    <h3 slot="heading">Change Your Email</h3>
+    <form action="?/updateEmail" method="POST" class="space-y-2">
+        <Input
+            id="email"
+            type="email"
+            required="true"
+            value={form?.data?.email}
+        />
+        <button type="submit" class="btn btn-primary w-full">Change My Email</button>
+    </form>
+  </Modal>
+</div>
